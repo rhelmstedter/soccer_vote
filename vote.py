ad877842c421a332087b4c8466b330b60fdb8823
Russell Helmstedter <rhelmstedter@gmail.com>
2026-02-04 21:48:17 -0800

    fixed script using playwright

--- vote.py
+++ vote.py
@@ -3,24 +3,25 @@ import time
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch()
     context = browser.new_context()
     page = context.new_page()
     try:
         page.goto("https://www.vcstar.com/story/sports/high-school/2026/02/03/vote-for-the-top-boys-soccer-player-in-the-area/88497911007/",
-                  timeout=3000)
+                  timeout=2000)
     except TimeoutError:
         pass
     page.mouse.wheel(0, 1000)
-    time.sleep(1)
+    time.sleep(.5)
     page.mouse.wheel(0, 1000)
+    iframe_locator = page.frame_locator(r'iframe[title="Embedded reader poll - Fans Choice: Who is Ventura County\'s No. 1 boys soccer player?"]')
+    iframe_locator.get_by_label('Cesar Fonseca, Del Sol').click()
+    iframe_locator.get_by_role("button").click()
+    print("voted")
     time.sleep(1)
-    page.mouse.wheel(0, 1000)
-    time.sleep(5)
-    page.get_by_role("radio", name="PDI_answer16573377").check()
-    page.locator("text=Vote").click()
-    # browser.close()
+    browser.close()
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+while True:
+    with sync_playwright() as playwright:
+        run(playwright)
